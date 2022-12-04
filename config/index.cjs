623c371c4b0d52e1e2795db05ea8acc46d1c11e2
z2789 <119793488+z2789@users.noreply.github.com>
2022-12-04 20:42:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,22 +29,22 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2002', date: '02-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '1999', date: '05-17',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-23',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-26' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       
       ],
     },
   ],
